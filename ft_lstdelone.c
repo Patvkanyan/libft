ffeb5bf4f4432220d10be6b9e92dc804f8acd8aa
Patvakanyan <alen.patvakanyan.2004@gmail.com>
2025-01-30 12:11:03 +0400

    add

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apatvaka <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: alen <alen@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 18:53:16 by apatvaka          #+#    #+#             */
-/*   Updated: 2025/01/27 19:39:48 by apatvaka         ###   ########.fr       */
+/*   Updated: 2025/01/30 12:10:20 by alen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,6 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*))
 	ptr = lst;
 	if (!lst)
 		return ;
-	while (ptr->next)
-	{
-		del(ptr->content);
-		ptr = ptr->next;
-	}
+	del(ptr->content);
 	free(lst);
 }
